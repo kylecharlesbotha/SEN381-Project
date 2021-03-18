1a92719c6ac3d4cdcbdb61d489ce0011a51effc1
zaneinsane <70311791+zaneinsane@users.noreply.github.com>
2021-03-18 08:53:59 +0200

    Create tblschdule
    Made tblschedule

---
+++ Database/SQL Query/tblSchdule.sql
@@ -0,0 +1,18 @@
+USE [Premier_Service_Solutions]
+GO
+
+/****** Object:  Table [dbo].[tblSchedule]    Script Date: 2021/03/18 08:51:06 ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE TABLE tblSchedule(
+	[ScheduleID] [int] PRIMARY KEY IDENTITY(1,1) NOT NULL,
+	[ScheduleDate] [date] NOT NULL,
+	[ScheduleStartTime] [time](7)NOT NULL,
+	[ScheduleEndTime] [time](7) NOT NULL,
+	[ScheduleStatus] [nchar](10) NOT NULL,
+	[TicketID] [int] NOT NULL,
+)
