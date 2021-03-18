cb60ece47eb2870fc84ad941e03b75a2b0fcbb82
Darren Oosthuizen <70310594+DarrenOosthuizen@users.noreply.github.com>
2021-03-18 09:10:46 +0200

    Update tblBusinessContact.sql

--- Database/SQL Query/tblBusinessContact.sql
+++ Database/SQL Query/tblBusinessContact.sql
@@ -1,17 +1,11 @@
 USE Premier_Service_Solution
 GO
 
-/****** Object:  Table [dbo].[tblBusinessContact]    Script Date: 2021/03/18 09:03:53 ******/
-SET ANSI_NULLS ON
-GO
 
-SET QUOTED_IDENTIFIER ON
-GO
-
-CREATE TABLE tblBusinessContact(
+CREATE TABLE dbo.tblBusinessContact(
 	BusinessID int PRIMARY KEY NOT NULL,
 	ClientID int FOREIGN KEY REFRENCES tblClient(ClientID) NOT NULL,
 	ContactJobTitle varchar(50) NOT NULL,
-	ContactState varchar(50) NOT NULL,
+	ContactState varchar(50) NOT NULL
 
 )
