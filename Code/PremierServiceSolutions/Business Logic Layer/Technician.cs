2d981597ba61fd07e4ea4add5cc50f594ff694d3
DarrenOosthuizen <70310594+DarrenOosthuizen@users.noreply.github.com>
2021-03-25 10:43:05 +0200

    Added Technician.cs

---
+++ Code/PremierServiceSolutions/Business Logic Layer/Technician.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PremierServiceSolutions.Business_Logic_Layer
+{
+    class Technician
+    {
+        private int technicianID;
+        private string technicianStatus;
+        private string technicianLevel;
+
+        public Technician(int technicianID, string technicianStatus, string technicianLevel)
+        {
+            this.technicianID = technicianID;
+            this.technicianStatus = technicianStatus;
+            this.technicianLevel = technicianLevel;
+        }
+
+        public Technician()
+        {
+        }
+
+        public int TechnicianID { get => technicianID; set => technicianID = value; }
+        public string TechnicianStatus { get => technicianStatus; set => technicianStatus = value; }
+        public string TechnicianLevel { get => technicianLevel; set => technicianLevel = value; }
+
+        public override string ToString()
+        {
+            return base.ToString();
+        }
+
+        public override bool Equals(object obj)
+        {
+            return base.Equals(obj);
+        }
+
+        public override int GetHashCode()
+        {
+            return base.GetHashCode();
+        }
+    }
+}
