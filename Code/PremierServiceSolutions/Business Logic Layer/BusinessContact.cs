ad36665415b6b5a272c3d15778a34338e0b920bc
Kyle Botha <576383@student.belgiumcampus.ac.za>
2021-04-05 18:58:28 +0200

    Added BusinessContact.cs class

--- Code/PremierServiceSolutions/Business Logic Layer/BusinessContact.cs
+++ Code/PremierServiceSolutions/Business Logic Layer/BusinessContact.cs
@@ -8,5 +8,31 @@ namespace PremierServiceSolutions.Business_Logic_Layer
 {
     class BusinessContact
     {
+        private int contactBusinessID;
+        private int contactClientID;
+        private int contactJobTitle;
+        private string contactState;
+
+        public BusinessContact(int contactBusinessID, int contactClientID, int contactJobTitle, string contactState)
+        {
+            this.contactBusinessID = contactBusinessID;
+            this.contactClientID = contactClientID;
+            this.contactJobTitle = contactJobTitle;
+            this.contactState = contactState;
+        }
+        public BusinessContact()
+        {
+
+        }
+
+        public int ContactBusinessID { get => contactBusinessID; set => contactBusinessID = value; }
+        public int ContactClientID { get => contactClientID; set => contactClientID = value; }
+        public int ContactJobTitle { get => contactJobTitle; set => contactJobTitle = value; }
+        public string ContactState { get => contactState; set => contactState = value; }
+
+        public void GetBusinessContact()
+        {
+
+        }
     }
 }
