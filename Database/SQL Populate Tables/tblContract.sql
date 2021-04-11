fdc7fae6425e714e7d7b2fea304e2bcd9603809e
zaneinsane <70311791+zaneinsane@users.noreply.github.com>
2021-04-11 20:00:55 +0200

    Add files via upload

---
+++ Database/SQL Populate Tables/tblContract.sql
@@ -0,0 +1,6 @@
+insert into tblContract (ContractDescription,ContractType) values ('Remote desktop help','Remote Services');
+insert into tblContract (ContractDescription,ContractType) values ('Physical respondants onsite','Onsite support');
+insert into tblContract (ContractDescription,ContractType) values ('Security support and update','Security support');
+insert into tblContract (ContractDescription,ContractType) values ('Support onsite and remote when needed','Hybrid support');
+insert into tblContract (ContractDescription,ContractType) values ('Hosting of websites','Web Hosting');
+insert into tblContract (ContractDescription,ContractType) values ('Package including all services','All services');
---
+++ Database/SQL Populate Tables/tblService.sql
@@ -0,0 +1,19 @@
+insert into tblService(ServiceDescription,ServiceLevel,ServiceName) values ('Level 1 Remote support','Level1','Remote Support');
+insert into tblService(ServiceDescription,ServiceLevel,ServiceName) values ('Level 2 Remote support','Level2','Remote Support');
+insert into tblService(ServiceDescription,ServiceLevel,ServiceName) values ('Level 3 Remote support','Level3','Remote Support');
+insert into tblService(ServiceDescription,ServiceLevel,ServiceName) values ('Level 1 Onsite Support','Level1','Onsite Support ');
+insert into tblService(ServiceDescription,ServiceLevel,ServiceName) values ('Level 2 Onsite Support','Level2','Onsite Support ');
+insert into tblService(ServiceDescription,ServiceLevel,ServiceName) values ('Level 3 Onsite Support','Level3','Onsite Support ');
+insert into tblService(ServiceDescription,ServiceLevel,ServiceName) values ('Level 1 Security Support','Level1','Security Support');
+insert into tblService(ServiceDescription,ServiceLevel,ServiceName) values ('Level 2 Security Support','Level2','Security Support');
+insert into tblService(ServiceDescription,ServiceLevel,ServiceName) values ('Level 3 Security Support','Level3','Security Support');
+insert into tblService(ServiceDescription,ServiceLevel,ServiceName) values ('Level 1 Hybrid Support','Level1','Hybrid Support');
+insert into tblService(ServiceDescription,ServiceLevel,ServiceName) values ('Level 2 Hybrid Support','Level2','Hybrid Support');
+insert into tblService(ServiceDescription,ServiceLevel,ServiceName) values ('Level 3 Hybrid Support','Level3','Hybrid Support');
+insert into tblService(ServiceDescription,ServiceLevel,ServiceName) values ('Level 1 Hosting  Support','Level1','Hosting Support');
+insert into tblService(ServiceDescription,ServiceLevel,ServiceName) values ('Level 2 Hosting  Support','Level2','Hosting Support');
+insert into tblService(ServiceDescription,ServiceLevel,ServiceName) values ('Level 3 Hosting  Support','Level3','Hosting Support');
+
+
+
+
