17061c7a39acc2eb6b604eb6c455c1790d52f92c
DarrenOosthuizen <70310594+DarrenOosthuizen@users.noreply.github.com>
2021-04-13 12:42:44 +0200

    Added Example Page

--- Code/PremierServiceSolutions/Data Access Layer/DBHandler.cs
+++ Code/PremierServiceSolutions/Data Access Layer/DBHandler.cs
@@ -1,12 +1,37 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Forms;
 
 namespace PremierServiceSolutions.Data_Access_Layer
 {
     class DBHandler
     {
+        private bool IsOnline = false;
+        private readonly string Connection = @"Data Source =flystudio.co.za\FLYSTUDIOSERVER ; Initial Catalog= Premier_Service_Solutions; User ID=PremierServiceSolutions ;Password =BelgiumCampus";
+
+        public string ConnectionVal { get => Connection; }
+
+        private void IsServerConnected(string ConnectionString)
+        {
+            using (SqlConnection sqlCon = new SqlConnection(ConnectionString))
+            {
+                try
+                {
+                    sqlCon.Open();
+                    IsOnline = true;
+                }
+                catch (SqlException e)
+                {
+                    MessageBox.Show(e.Message);
+                    IsOnline = false;
+                }
+
+            }
+        }
+
     }
 }
---
+++ Code/PremierServiceSolutions/Data Access Layer/TestExampleDH.cs
@@ -0,0 +1,42 @@
+﻿using PremierServiceSolutions.Business_Logic_Layer;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PremierServiceSolutions.Data_Access_Layer
+{
+    class TestExampleDH
+    {
+        DBHandler objHandler = new DBHandler();
+
+
+        private void Run(Employee objEmp)
+        {
+            try
+            {
+                SqlConnection sqlCon = new SqlConnection(objHandler.ConnectionVal);
+
+                string InsertQuery = string.Format(@"INSERT INTO tblEmployee (Field1, Field2, and so forth) VALUES ('{0}','{1}')", objEmp.EmployeeCell, objEmp.EmployeeRole);
+                SqlCommand InsertCommand = new SqlCommand(InsertQuery, sqlCon);
+                sqlCon.Open();
+                InsertCommand.ExecuteNonQuery();
+                sqlCon.Close();
+
+
+                string UpdateQuery = string.Format(@"UPDATE tblEmployee SET Field1 ='{0}' WHERE Field2 ='{1}'", objEmp.EmployeeStatus, objEmp.EmployeeStart);
+                SqlCommand UpdateCommand = new SqlCommand(UpdateQuery, sqlCon);
+                sqlCon.Open();
+                UpdateCommand.ExecuteNonQuery();
+                sqlCon.Close();
+            }
+            catch (SqlException e)
+            {
+                MessageBox.Show(e.Message);
+            }
+        }
+    }
+}
