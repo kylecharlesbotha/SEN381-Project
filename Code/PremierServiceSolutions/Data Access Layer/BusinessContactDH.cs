2ffe40375cb78d50f02dcd399221a024cebcbb96
DarrenOosthuizen <70310594+DarrenOosthuizen@users.noreply.github.com>
2021-04-13 21:44:06 +0200

    Added BusinessContactDh and BusinessDH files

--- Code/PremierServiceSolutions/Data Access Layer/BusinessContactDH.cs
+++ Code/PremierServiceSolutions/Data Access Layer/BusinessContactDH.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using PremierServiceSolutions.Business_Logic_Layer;
+using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,5 +10,99 @@ namespace PremierServiceSolutions.Data_Access_Layer
 {
     class BusinessContactDH
     {
+        //Object of DBHandler which will store the connection string. We do this so that we dont have to repeat code in multiple classes but instead just one
+        DBHandler objHandler = new DBHandler();
+
+        //Method which will be used to create new record
+        private bool CreateBusinessContact(BusinessContact objBusCon)
+        {
+            try 
+            {
+                //First Check if the record already exsists by calling  FindBusinessContact()
+                //If found return false and display a message that it already exists else continue with creating
+                //Then you will need to check if the Client Record Exists and also if the Business Record Exists. Call CheckAllTables() as that will check all the tables it needs to
+                return true;
+            }
+            catch (SqlException SQLE)
+            {
+                return false;
+            }
+            
+        }
+
+        //Method which will be used to Update current record within Database
+        private bool UpdateBusinessContact(BusinessContact newObjBusCon, BusinessContact oldObjBusCon)
+        {
+            try
+            {
+                //Update to new records where it matches the old BusinessID and ClientID
+                return true;
+            }
+            catch (SqlException SQLE)
+            {
+                return false;
+            }
+        }
+
+        //Method used to Delete a record from the database
+        private bool DeleteBusinessContact(BusinessContact objBusCon)
+        {
+            try
+            {
+                //Dont delete the record instead change the state of the record to 0 now as that will be inactive records
+                return true;
+            }
+            catch (SqlException SQLE)
+            {
+                return false;
+            }
+        }
+        //Method used to Get all the records from the table in the database
+        private List<BusinessContact> GetAllBusinessContact(BusinessContact objBusCon)
+        {
+            //List of type BusinessContact which will store all the records and then return that list
+            List<BusinessContact> allBusCon = new List<BusinessContact>();
+            
+            try
+            {
+                //Return all records from table
+                return allBusCon;
+            }
+            catch(SqlException SQLE)
+            {
+                return allBusCon;
+            }
+        }
+
+        //Method used to find one record within the table
+        private BusinessContact FindBusinessContact(BusinessContact objBusCon)
+        {
+            //Object of type BusinessContact which will store single record of object to be returned to interface
+            BusinessContact objRecord = new BusinessContact();
+            try
+            {
+                //Find record matching to obj and return it in objRecord
+                return objRecord;
+            }
+            catch (SqlException SQLE)
+            {
+                return objRecord;
+            }
+        }
+
+        //Method which will be called to check that neccessary fields exist in the other tables which have relationships
+        private bool CheckAllTables(BusinessContact objBusCon)
+        {
+            try
+            {
+                //Check if it exists in tblClient first then if it does continue with checking tblBusiness.
+                //If it is not found in tblClient then return false. If it is found within tblBusiness return true else return false
+                return true;
+            }
+            catch (SqlException SQLE)
+            {
+                return false;
+            }
+        }
     }
 }
--- Code/PremierServiceSolutions/Data Access Layer/BusinessDH.cs
+++ Code/PremierServiceSolutions/Data Access Layer/BusinessDH.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using PremierServiceSolutions.Business_Logic_Layer;
+using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,5 +10,86 @@ namespace PremierServiceSolutions.Data_Access_Layer
 {
     class BusinessDH
     {
+        //Object of DBHandler which will store the connection string. We do this so that we dont have to repeat code in multiple classes but instead just one
+        DBHandler objHandler = new DBHandler();
+
+        //Method which will be used to create new record
+        private bool CreateBusiness(Business objBus)
+        {
+            try
+            {
+                //First Check if the record already exsists by calling  FindBusiness()
+                //If found return false and display a message that it already exists else continue with creating
+                
+                return true;
+            }
+            catch (SqlException SQLE)
+            {
+                return false;
+            }
+
+        }
+
+        //Method which will be used to Update current record within Database
+        private bool UpdateBusiness(Business newObjBus, Business oldObjBus)
+        {
+            try
+            {
+                //Update to new records where it matches the old BusinessID
+                return true;
+            }
+            catch (SqlException SQLE)
+            {
+                return false;
+            }
+        }
+
+        //Method used to Delete a record from the database
+        private bool DeleteBusiness(Business objBus)
+        {
+            try
+            {
+                //Dont delete the record instead change the state of the record to 0 now as that will be inactive records
+                return true;
+            }
+            catch (SqlException SQLE)
+            {
+                return false;
+            }
+        }
+        //Method used to Get all the records from the table in the database
+        private List<Business> GetAllBusiness(Business objBus)
+        {
+            //List of type Business which will store all the records and then return that list
+            List<Business> allBus = new List<Business>();
+
+            try
+            {
+                //Return all records from table
+                return allBus;
+            }
+            catch (SqlException SQLE)
+            {
+                return allBus;
+            }
+        }
+
+        //Method used to find one record within the table
+        private Business FindBusiness(Business objBus)
+        {
+            //Object of type Business which will store single record of object to be returned to interface
+            Business objRecord = new Business();
+            try
+            {
+                //Find record matching to obj and return it in objRecord
+                return objRecord;
+            }
+            catch (SqlException SQLE)
+            {
+                return objRecord;
+            }
+        }
+
+
     }
 }
