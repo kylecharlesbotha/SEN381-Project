26f8e235289e41dd508389cf95036f8e3879261e
ClaudePretorius <clde.pretorius@gmail.com>
2021-04-16 19:38:37 +0200

    Updated BusinessDH

--- Code/PremierServiceSolutions/Data Access Layer/BusinessDH.cs
+++ Code/PremierServiceSolutions/Data Access Layer/BusinessDH.cs
@@ -5,6 +5,7 @@ using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Forms;
 
 namespace PremierServiceSolutions.Data_Access_Layer
 {
@@ -86,6 +87,47 @@ namespace PremierServiceSolutions.Data_Access_Layer
             catch (SqlException SQLE)
             {
                 return 0;
+            }
+        }
+
+        private Business GetBusiness(Business objBus)
+        {
+            Business objRecord = new Business();
+            try
+            {
+                //List of type Business which will store all the records and then return that list
+                List<Business> allBusiness = new List<Business>();
+                //New SQL Connection which the query will use to perform the Select of tblBusiness
+                SqlConnection sqlCon = new SqlConnection(objHandler.ConnectionVal);
+                //Select Query which will store the SQL qeury needed to return all the Business
+                string SelectQuery = string.Format("SELECT * FROM tblBusiness WHERE BusinessID = '{0}'", objBus.BusinessID);
+                //New Command which will take in the sqlCon and UpdateQuery var
+                SqlCommand sqlCommand = new SqlCommand(SelectQuery, sqlCon);
+                //SQL Datareader which will be used to pull specific fields from the Select Return statement
+                SqlDataReader sqlDataReader;
+                //Open the connection to the database
+                sqlCon.Open();
+                //
+                sqlDataReader = sqlCommand.ExecuteReader();
+                while (sqlDataReader.Read())
+                {
+                    objRecord.BusinessID = (int)sqlDataReader.GetValue(0);
+                    objRecord.BusinessAddress = (string)sqlDataReader.GetValue(1);
+                    objRecord.BusinessName = (string)sqlDataReader.GetValue(2);
+                    objRecord.BusinessPhone = (string)sqlDataReader.GetValue(3);
+                    objRecord.BusinessRegistrationNumber = (int)sqlDataReader.GetValue(4);
+                    objRecord.BusinessState = (string)sqlDataReader.GetValue(5);
+                }
+                //Close connection to database
+                sqlCon.Close();
+                //Return List of Technicians
+                return objRecord;
+            }
+            catch (SqlException SQLE)
+            {
+                //Will catch any errors that occur and will display a error message. it will also return a empty list
+                MessageBox.Show("Error has occured");
+                return null;
             }
         }
 
