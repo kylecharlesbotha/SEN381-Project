cc79c0c4f57e821b09d103ca8d65dc92953c10ae
Kyle Botha <576383@student.belgiumcampus.ac.za>
2021-05-01 14:18:25 +0200

    Add error handling middleware for satisfaction routes

--- InternalDatabaseAPI/src/routers/satisfaction.js
+++ InternalDatabaseAPI/src/routers/satisfaction.js
@@ -1,15 +1,29 @@
 const router = require("express").Router();
 const Crud = require("../models/crud");
 SatisfactionCrud = new Crud("tblSatisfaction", "SatisfactionID");
-router.get("/db/satisfaction/getSatisfactions",async(req,res)=>{
-    res.send(await SatisfactionCrud.readAll());  
+router.get("/db/satisfaction/getSatisfactions",async(req,res,next)=>{
+    try{
+        res.send(await SatisfactionCrud.readAll()); 
+    }catch (error){
+        next(error);
+    }
 })
-router.post("/db/satisfaction/getSatisfaction",async(req,res)=>{
+router.post("/db/satisfaction/getSatisfaction",async(req,res,next)=>{
     //!Expects userid header inside post req
-    res.send(await SatisfactionCrud.readOne(req.headers.userid));  
+    try{
+        res.send(await SatisfactionCrud.readOne(req.headers.userid));
+    }catch (error){
+        next(error);
+    }
+      
 })
-router.post("/db/satisfaction/updateSatisfaction",async(req,res)=>{
-    res.send(await SatisfactionCrud.updateOne(req.headers.userid, req.body.changes));
+router.post("/db/satisfaction/updateSatisfaction",async(req,res,next)=>{
+    try{
+        res.send(await SatisfactionCrud.updateOne(req.headers.userid, req.body.changes));
+    }catch (error){
+        next(error);
+    }
+    
 })
 
 module.exports = router;
