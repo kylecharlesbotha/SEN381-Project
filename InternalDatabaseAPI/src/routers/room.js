42132b316c64f0177e5f2208e8dfb036627b2a72
Kyle Botha <576383@student.belgiumcampus.ac.za>
2021-05-01 14:22:11 +0200

    Add error handling middleware for satisfaction

--- InternalDatabaseAPI/src/routers/room.js
+++ InternalDatabaseAPI/src/routers/room.js
@@ -2,14 +2,26 @@
 const router = new require("express").Router()
 const Crud = require("../models/crud");
 RoomCrud = new Crud("tblRoom", "RoomID");     
-router.get("/db/rooms/getRooms",async(req,res)=>{
-    res.send(await RoomCrud.readAll());
+router.get("/db/rooms/getRooms",async(req,res,next)=>{
+    try{
+        res.send(await RoomCrud.readAll());
+    }catch(error){
+        next(error); 
+    }
 })
-router.post("/db/rooms/getRoom",async(req,res)=>{
-    res.send(await RoomCrud.readOne(req.headers.roomid)); 
+router.post("/db/rooms/getRoom",async(req,res,next)=>{
+    try{
+        res.send(await RoomCrud.readOne(req.headers.roomid)); 
+    }catch(error){
+        next(error); 
+    }
 })
-router.post("/db/rooms/updateRoom",async(req,res)=>{
-    res.send(await RoomCrud.updateOne(req.headers.roomid, req.body.changes));
+router.post("/db/rooms/updateRoom",async(req,res,next)=>{
+    try{
+        res.send(await RoomCrud.updateOne(req.headers.roomid, req.body.changes));
+    }catch(error){
+        next(error); 
+    }
 })
 
 module.exports = router
