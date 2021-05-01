53e84ae0c88a1ec69694d2d57b777442db7b27a0
Kyle Botha <576383@student.belgiumcampus.ac.za>
2021-05-01 14:25:59 +0200

    Add error handling middleware for employee routes

--- InternalDatabaseAPI/src/routers/employee.js
+++ InternalDatabaseAPI/src/routers/employee.js
@@ -1,14 +1,26 @@
 const router = new require("express").Router()
 const Crud = require("../models/crud");
 EmployeeCRUD = new Crud("tblEmployee", "EmployeeID");
-router.get("/db/employees/getEmployees",async(req,res)=>{
-    res.send(await EmployeeCRUD.readAll());
+router.get("/db/employees/getEmployees",async(req,res,next)=>{
+    try{
+        res.send(await EmployeeCRUD.readAll());
+    }catch(error){
+        next(error);
+    }    
 })
 router.post("/db/employees/getEmployee",async(req,res)=>{
-    res.send(await EmployeeCRUD.readOne(req.headers.messageid)); 
+    try{
+        res.send(await EmployeeCRUD.readOne(req.headers.messageid));
+    }catch(error){
+        next(error);
+    }
 })
 router.post("/db/employees/updateEmployee",async(req,res)=>{
-    res.send(await EmployeeCRUD.updateOne(req.headers.messageid, req.body.changes));
+    try{
+        res.send(await EmployeeCRUD.updateOne(req.headers.messageid, req.body.changes));
+    }catch(error){
+        next(error);
+    }
 })
 
 module.exports = router
