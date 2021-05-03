6d93797cfcb40411e233ac3ca4874e65763cd46f
Kyle Botha <576383@student.belgiumcampus.ac.za>
2021-05-03 13:59:26 +0200

    Harder font weight on navbar li

--- Web App/Client/src/components/Home/Navigation/Navigation.js
+++ Web App/Client/src/components/Home/Navigation/Navigation.js
@@ -1,56 +1,79 @@
 import "./Navigation.css";
 import { NavLink } from "react-router-dom";
+import Typography from '@material-ui/core/Typography';
+import Box from '@material-ui/core/Box';
 const Navigation = (props) => {
   return (
-    <nav className="navbar navbar-expand-lg navbar-light bg-light shadow-lg p-2 mb-2 bg-white rounded">
-      <div className="container-fluid">
-        <a className="navbar-brand" href="/">
-          Premier Service Solutions
-        </a>
-        <button
-          className="navbar-toggler"
-          type="button"
-          data-bs-toggle="collapse"
-          data-bs-target="#navbarNav"
-        >
-          <i className="fa fa-bars"></i>
-        </button>
-        <div className="collapse navbar-collapse" id="navbarNav">
-          <ul className="navbar-nav ms-auto">
-            <li className="nav-item">
-              <a className="nav-link active" aria-current="page" href="/">
-                Home
-              </a>
-            </li>
-            <li className="nav-item scroll-to-section">
-              <a className="nav-link" href="/">
-                About Us
-              </a>
-            </li>
-            <li className="nav-item">
-              <a className="nav-link" href="/">
-                Services
-              </a>
-            </li>
-            <li className="nav-item">
-              <a className="nav-link" href="/">
-                Contact Us
-              </a>
-            </li>
-            <li className="nav-item">
-              <NavLink className="nav-link" to="ClientPortal">
-                Client Portal
-              </NavLink>
-            </li>
-            <li className="nav-item">
-              <NavLink className="nav-link" to="AgentPortal">
-                Agent Portal
-              </NavLink>
-            </li>
-          </ul>
-        </div>
-      </div>
-    </nav>
+    <Typography component="Div" >
+      <Box>
+        <nav  className="navbar navbar-expand-lg navbar-light bg-light shadow-lg p-2 mb-2 bg-white rounded">
+          <div className="container-fluid">
+            <a  className="navbar-brand" href="/">
+              Premier Service Solutions
+            </a>
+            <button
+              className="navbar-toggler"
+              type="button"
+              data-bs-toggle="collapse"
+              data-bs-target="#navbarNav"
+            >
+              <i className="fa fa-bars"></i>
+            </button>
+              <div className="collapse navbar-collapse" id="navbarNav">
+                
+                <ul className="navbar-nav ms-auto">
+                  <li fontWeight="fontWeightBold" className="nav-item">
+                    <Box fontWeight="fontWeightBold" m={1}>
+                      <a className="nav-link active" aria-current="page" href="/">
+                      Home
+                      </a>
+                    </Box>
+                    
+                  </li>
+                  <li className="nav-item scroll-to-section">
+                    <Box fontWeight="fontWeightBold" m={1}><a className="nav-link" href="/">
+                      About Us
+                    </a></Box>
+                    
+                  </li>
+                  <li className="nav-item">
+                    <Box fontWeight="fontWeightBold" m={1}>
+                      <a className="nav-link" href="/">
+                        Services
+                      </a>
+                    </Box>
+                    
+                  </li>
+                  <li className="nav-item">
+                    <Box fontWeight="fontWeightBold" m={1}>
+                      <a className="nav-link" href="/">
+                      Contact Us
+                      </a>
+                    </Box>
+                    
+                  </li>
+                  <li className="nav-item">
+                    <Box fontWeight="fontWeightBold" m={1}>
+                    <NavLink  className="nav-link" to="ClientPortal">
+                      Client Portal
+                    </NavLink>
+
+                    </Box>
+                    
+                  </li>
+                  <li className="nav-item">
+                    <Box fontWeight="fontWeightBold" m={1}><NavLink className="nav-link" to="AgentPortal">
+                      Agent Portal
+                    </NavLink></Box>
+                    
+                  </li>
+                </ul>
+              </div>
+            </div>
+          </nav>
+        </Box>
+    </Typography>
+    
   );
 };
 
