2233f172fa58975393793687fd29c53cacd47bc6
Kyle Botha <576383@student.belgiumcampus.ac.za>
2021-05-05 12:50:29 +0200

    Add get tickets private route

--- Web App/Server/src/controllers/private.js
+++ Web App/Server/src/controllers/private.js
@@ -1,6 +1,17 @@
-exports.getPrivateData = (req,res,next) => {
+const getAllTickets = require("../models/Ticket");
+const getPrivateData = (req,res,next) => {
     res.status(200).json({
         success: true, 
         data: "This is a private route for employees only."
     })
 }
+
+const getTickets = async(req,res,next) => {
+    const tickets = await getAllTickets();
+    res.status(200).json({
+        success: true, 
+        data: tickets
+    })
+}
+
+module.exports = {getPrivateData, getTickets}
