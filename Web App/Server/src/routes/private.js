320c4e8d284617691dfe6efb90aae2ef56f454bd
Kyle Botha <576383@student.belgiumcampus.ac.za>
2021-05-05 12:51:54 +0200

    Addprivate tickets route

--- Web App/Server/src/routes/private.js
+++ Web App/Server/src/routes/private.js
@@ -1,6 +1,7 @@
 const express = require("express"); 
 const router = express.Router(); 
-const {getPrivateData} = require("../controllers/private"); 
+const {getPrivateData, getTickets} = require("../controllers/private"); 
 const {protect} = require("../middleware/auth");
 router.route("/").get(protect,getPrivateData); 
+router.route("/data/tickets").get(protect,getTickets); 
 module.exports = router;
