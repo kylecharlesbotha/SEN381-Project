c11c299fb279c9102adb20ace545d7140af1f756
Kyle Botha <576383@student.belgiumcampus.ac.za>
2021-05-05 12:48:14 +0200

    Dynamically load tickets

--- Web App/Client/src/components/AgentPortalDashboard/AgentPortalDashboard.js
+++ Web App/Client/src/components/AgentPortalDashboard/AgentPortalDashboard.js
@@ -1,8 +1,11 @@
-import React from "react";
+import React, { useEffect, useState } from 'react';
 import Aux from "../../hoc/Auxillary";
-
+import Ticket from "./Ticket/Ticket.js"
+import axios from "axios";
 import { makeStyles } from '@material-ui/core/styles';
 import { colors } from "@material-ui/core";
+
+
 const useStyles = makeStyles((theme) => ({
     root: {
       '& > *': {
@@ -14,32 +17,69 @@ const useStyles = makeStyles((theme) => ({
     mainDashboardContainer: {
         margin: "0px",
         width: '100%',
-       // backgroundColor: colors.blue[100],
-    },leftPanel: {
-       // backgroundColor: colors.blue[200],
-        height: "100vh"
-    },panelTitle: {
-        //backgroundColor: colors.red[200],
+        height: "100vh",
+        //backgroundColor: colors.blue[100],
+    },mainContainer: {
+       
     }
   }));
-const menuItem = ["Dashboard", "Tickets", "Chats"]; 
+
 const Dashboard = (props) => {
+    const [tickets, setTickets] = useState([]);
+    const [error, setError] = useState([]);
+    const truncate = (string) => {
+        return string.length > 48 ? string.substring(0, 45) + "..." : string;  
+    }
+    useEffect(()=>{
+        const config = {
+            headers: {
+                "Content-Type": "application/json",
+                Authorization: `Bearer ${localStorage.getItem("authToken")}`,
+            },
+        };
+        try { 
+            axios.get(
+                "http://41.1.73.25:3001/api/private/data/tickets",
+                config
+            ).then(res => {     
+                setTickets(res.data.data.recordset);
+            })
+            
+        } catch (error) {
+         console.dir(error);
+            setError(error.response.data.error);
+            setTimeout(() => {
+                setError("");
+            }, 5000);
+        }
+    }, []);
+    
+    
+     
+    
+        
+     
+   
     const classes = useStyles();
+    
+
     return (
         <Aux>
-        <div className={`container-fluid ${classes.mainDashboardContainer}`}>
-            <div className={`row`}>
-                <div className={`col-md-2 shadow-md ${classes.leftPanel}`}>
-                    <div className={`row text-center pt-4 pb-3 shadow-sm  ${classes.panelTitle}`}>
-                        <h3>Dashboard</h3>
+            {/* //<Ticket title={ticket.TicketTitle} priority={ticket.TicketPriority} description={ticket.TicketDescription}/> */}
+            
+            <div className={`container mt-4 `}>
+                <div className={`row justify-content-center ${classes.mainContainer}`}>
+                {tickets.map(ticket => (
+                    <div className={`col-md-5 ${classes.ticketsColumn}`}>
+                        <div>                                       
+                            <Ticket key={ticket.TicketID} title={ticket.TicketTitle} priority={ticket.TicketPriority} description={truncate(ticket.TicketDescription)}/>                 
+                        </div>  
+                                
                     </div>
-                </div>
-                <div className={`col-md-9 offset-sm-1 shadow-lg ${classes.leftPanel}`}>
-                    <h2>Welcome to the Agent Portal!</h2>
+                    ))}
                 </div>
             </div>
-        </div>
-    </Aux>
+        </Aux>
     );
   };
-export default Dashboard;
+export default Dashboard;                   
