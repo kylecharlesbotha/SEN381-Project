f7cf03d9fde0a247e1ddfb5cb76d643d78e708b6
Kyle Botha <576383@student.belgiumcampus.ac.za>
2021-05-06 23:24:35 +0200

    Refactored agent portal dash

--- Web App/Client/src/components/AgentPortalDashboard/AgentPortalDashboard.js
+++ Web App/Client/src/components/AgentPortalDashboard/AgentPortalDashboard.js
@@ -1,58 +1,58 @@
 import React, { useEffect, useState } from 'react';
 import Aux from "../../hoc/Auxillary";
-import Ticket from "./Ticket/Ticket.js"
+//import Ticket from "../Ticket/Ticket.js"
 import axios from "axios";
 import { makeStyles } from '@material-ui/core/styles';
 import { colors } from "@material-ui/core";
 
 
-const useStyles = makeStyles((theme) => ({
-    root: {
-      '& > *': {
-        margin: theme.spacing(1),
-        width: '100%',
-       // backgroundColor: colors.blue[100],
-      }
-    },
-    mainDashboardContainer: {
-        margin: "0px",
-        width: '100%',
-        height: "100vh",
-        //backgroundColor: colors.blue[100],
-    },mainContainer: {
+// const useStyles = makeStyles((theme) => ({
+//     root: {
+//       '& > *': {
+//         margin: theme.spacing(1),
+//         width: '100%',
+//        // backgroundColor: colors.blue[100],
+//       }
+//     },
+//     mainDashboardContainer: {
+//         margin: "0px",
+//         width: '100%',
+//         height: "100vh",
+//         //backgroundColor: colors.blue[100],
+//     },mainContainer: {
        
-    }
-  }));
+//     }
+//   }));
 
 const Dashboard = (props) => {
-    const [tickets, setTickets] = useState([]);
-    const [error, setError] = useState([]);
-    const truncate = (string) => {
-        return string.length > 48 ? string.substring(0, 45) + "..." : string;  
-    }
-    useEffect(()=>{
-        const config = {
-            headers: {
-                "Content-Type": "application/json",
-                Authorization: `Bearer ${localStorage.getItem("authToken")}`,
-            },
-        };
-        try { 
-            axios.get(
-                "http://41.1.73.25:3001/api/private/data/tickets",
-                config
-            ).then(res => {     
-                setTickets(res.data.data.recordset);
-            })
+    // const [tickets, setTickets] = useState([]);
+    // const [error, setError] = useState([]);
+    // const truncate = (string) => {
+    //     return string.length > 48 ? string.substring(0, 45) + "..." : string;  
+    // }
+    // useEffect(()=>{
+    //     const config = {
+    //         headers: {
+    //             "Content-Type": "application/json",
+    //             Authorization: `Bearer ${localStorage.getItem("authToken")}`,
+    //         },
+    //     };
+    //     try { 
+    //         axios.get(
+    //             "http://41.1.73.25:3001/api/private/data/tickets",
+    //             config
+    //         ).then(res => {     
+    //             setTickets(res.data.data.recordset);
+    //         })
             
-        } catch (error) {
-         console.dir(error);
-            setError(error.response.data.error);
-            setTimeout(() => {
-                setError("");
-            }, 5000);
-        }
-    }, []);
+    //     } catch (error) {
+    //      console.dir(error);
+    //         setError(error.response.data.error);
+    //         setTimeout(() => {
+    //             setError("");
+    //         }, 5000);
+    //     }
+    // }, []);
     
     
      
@@ -60,14 +60,13 @@ const Dashboard = (props) => {
         
      
    
-    const classes = useStyles();
-    
+    //const classes = useStyles();
 
     return (
         <Aux>
             {/* //<Ticket title={ticket.TicketTitle} priority={ticket.TicketPriority} description={ticket.TicketDescription}/> */}
-            
-            <div className={`container mt-4 `}>
+            <h1>Dash</h1>
+            {/* <div className={`container mt-4 `}>
                 <div className={`row justify-content-center ${classes.mainContainer}`}>
                 {tickets.map(ticket => (
                     <div className={`col-md-5 ${classes.ticketsColumn}`}>
@@ -78,7 +77,7 @@ const Dashboard = (props) => {
                     </div>
                     ))}
                 </div>
-            </div>
+            </div> */}
         </Aux>
     );
   };
