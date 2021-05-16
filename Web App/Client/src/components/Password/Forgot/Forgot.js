abb43a33ba0eeb9662a7bbc43052220a4512f624
DarrenOosthuizen <70310594+DarrenOosthuizen@users.noreply.github.com>
2021-05-16 20:01:11 +0200

    Designed Forgot Form

--- Web App/Client/src/components/Password/Forgot/Forgot.js
+++ Web App/Client/src/components/Password/Forgot/Forgot.js
@@ -6,10 +6,12 @@ import Box from "@material-ui/core/Box";
 import Logo from "../../../assets/images/PSSLogo.png";
 import { NavLink } from "react-router-dom";
 import { useHistory } from "react-router-dom";
+import TextField from "@material-ui/core/TextField";
 
-function Forgot({ props }) {
+const Forgot = ({ props }) => {
   const browhistory = useHistory();
   console.log(browhistory);
+
   return (
     <Aux>
       <Typography component="div">
@@ -29,10 +31,57 @@ function Forgot({ props }) {
             </div>
           </nav>
         </Box>
-        <p></p>
+        <div className="input-group-prepend"></div>
+        <div className="container justify-content-center">
+          <div className="row justify-content-center">
+            <div className="col-md-7 forgotemail">
+              <h2>Are you having trouble signing in?</h2>
+              <p className="emailp">Please enter your email linked to your account.</p>
+              <TextField
+                id="filled-basic Subject"
+                label="Email Address"
+                required
+                name="Emaill Address"
+                variant="outlined"
+                className=" lblHeader"
+                size="small"
+                color="secondary"
+                style ={{width: '75%'}}
+                
+              />
+              <p className="infop">
+                We will send you a email to reset the password for your account
+                so you can continue.
+              </p>
+
+              <button
+                type="submit"
+                className="btn btnsignin justify-content-center"
+                color="#ff5c5c"
+              >
+                SEND EMAIL
+              </button>
+            </div>
+            <div className="col-md-3 forgottext">
+              <p>
+                Don't worry! You have come to the right place if you have
+                forgotten your email or password for your account. For security
+                purposes we ask that you please provide us your email so we may
+                send a email to you with instructions to reset your account
+                password
+              </p>
+            </div>
+          </div>
+        </div>
+        <div className="text-center p-3 footer">
+          © 2021 Copyright:
+          <a className="text-dark" href="https://premierservicesolutions.co.za">
+            PremierServiceSolutions
+          </a>
+        </div>
       </Typography>
     </Aux>
   );
-}
+};
 
 export default Forgot;
