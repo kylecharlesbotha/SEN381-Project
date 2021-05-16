8a5aefc8e83a0adf818429f36911fd56685124a5
Kyle Botha <576383@student.belgiumcampus.ac.za>
2021-05-16 15:30:21 +0200

    Create auth routes for loging in client

--- Web App/Server/src/controllers/auth.js
+++ Web App/Server/src/controllers/auth.js
@@ -1,6 +1,6 @@
-const {User, findOne} = require('../models/User'); 
+const {User, findOneEmployee, findOneClient} = require('../models/User'); 
 const ErrorResponse = require('../utils/errorResponse');
-const axios = require("axios");
+
 exports.register = async (req, res, next) => {
     const {username, email, password} = req.body;
     try{
@@ -15,7 +15,7 @@ exports.register = async (req, res, next) => {
     }
 };
 
-exports.login = async (req, res, next) => {
+exports.loginEmployee = async (req, res, next) => {
     const {UserName, UserPassword} = req.body; 
 
     if (!UserName || !UserPassword){
@@ -24,7 +24,7 @@ exports.login = async (req, res, next) => {
 
     try{
         //!Get user from db
-        var userobj = await findOne(UserName)
+        var userobj = await findOneEmployee(UserName)
          
         const user = new User(userobj);
         //var user = await User.findOne({email}).select("+password");
@@ -32,6 +32,40 @@ exports.login = async (req, res, next) => {
             return next(new ErrorResponse("Authentication failed", 401)); 
         }
         const isMatch = await user.matchPasswords(UserPassword); 
+        if (!isMatch){
+            return next(new ErrorResponse("Authentication failed", 401)); 
+        }
+        sendToken(user, 200, res);
+         
+    }catch(error){
+        console.log(error);
+        next(error);
+    };
+};
+exports.loginClient = async (req, res, next) => {
+    console.log("logging client in")
+    const {UserName, UserPassword} = req.body; 
+
+    if (!UserName || !UserPassword){
+        return next(new ErrorResponse("Authentication failed", 400)); 
+    };
+    console.log("1")
+    try{
+        //!Get user from db
+        var userobj = await findOneClient(UserName)
+    console.log("2")
+         
+        const user = new User(userobj);
+        console.log("3")
+
+        //var user = await User.findOne({email}).select("+password");
+        if (!user){
+            return next(new ErrorResponse("Authentication failed", 401)); 
+        }
+        console.log("4")
+        
+        const isMatch = await user.matchClientPasswords(UserPassword); 
+        console.log("5")
         if (!isMatch){
             return next(new ErrorResponse("Authentication failed", 401)); 
         }
