8101ff161d61e97f30026052c337f9f23a626ac0
Kyle Botha <576383@student.belgiumcampus.ac.za>
2021-05-16 15:31:07 +0200

    Add middleware for satisfactions functionality

--- Web App/Server/src/controllers/private.js
+++ Web App/Server/src/controllers/private.js
@@ -1,11 +1,18 @@
-const getAllTickets = require("../models/Ticket");
-const getPrivateData = (req,res,next) => {
+const {getAllTickets, getTicketByID} = require("../models/Ticket");
+const getAllSatisfactions = require("../models/Satisfaction");
+const getClientByID = require("./../models/Client");
+const getPrivateEmployeeData = (req,res,next) => {
     res.status(200).json({
         success: true, 
         data: "This is a private route for employees only."
     })
 }
-
+const getPrivateClientData = (req,res,next) => {
+    res.status(200).json({
+        success: true, 
+        data: "This is a private route for clients only."
+    })
+}
 const getTickets = async(req,res,next) => {
     const tickets = await getAllTickets();
     res.status(200).json({
@@ -13,5 +20,28 @@ const getTickets = async(req,res,next) => {
         data: tickets
     })
 }
-
-module.exports = {getPrivateData, getTickets}
+const getTicket = async(req,res,next) => {
+    
+    const ticket = await getTicketByID(req.body.TicketID);
+    res.status(200).json({
+        success: true, 
+        data: ticket
+    })
+}
+const getSatisfactions = async(req,res,next) => {
+    const satisfactions = await getAllSatisfactions();
+    res.status(200).json({
+        success: true, 
+        data: satisfactions
+    })
+}
+const getClient = async(req,res,next) => {
+    console.log(req.body.ClientID)
+    const client = await getClientByID(req.body.ClientID);
+    console.dir(client)
+    res.status(200).json({
+        success: true, 
+        data: client
+    })
+}
+module.exports = {getPrivateEmployeeData, getPrivateClientData, getTickets, getTicket, getSatisfactions, getClient}
