048fbec6d2b95de98425a92704d4b5b903f7eb51
Kyle Botha <576383@student.belgiumcampus.ac.za>
2021-05-16 15:33:59 +0200

    Add functions to find client details by ID

--- Web App/Server/src/models/User.js
+++ Web App/Server/src/models/User.js
@@ -2,37 +2,25 @@ const axios = require("axios");
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 class User{
-    // #username_ = null; 
-    // #password_ = null;
     constructor(UserObj){
         this.UserObj = UserObj;
     }
     
-    // getUsername(){
-    //     return this.#username_; 
-    // }
-    // getPassword(){
-    //     return this.#password_;
-    // }
-    // setUsername(username){
-    //     this.#username_ = username; 
-    // }
-    // setPassword(password){
-    //     this.#password_ = password; 
-    // }
     Create(userObj){
         //!Create user in db from here.
     }
     
     async matchPasswords(password){
-        
         return await bcrypt.compare(password, this.UserObj.UserPassword); 
+    } 
+    async matchClientPasswords(password){
+        return await bcrypt.compare(password, this.UserObj.ClientPassWord); 
     } 
     getSignedToken(){
         return jwt.sign({id: this.UserObj.UserID}, process.env.JWT_SECRET);
     }
 }
-const findOne = async(username) => {
+const findOneEmployee = async(username) => {
     console.log(`Finding user with username ${username}`); 
     const config = {
         header: {
@@ -51,7 +39,26 @@ const findOne = async(username) => {
         return error;
     }
 }
-const findById = async(id) => {
+const findOneClient = async(username) => {
+    console.log(`Finding user with username ${username}`); 
+    const config = {
+        header: {
+            "Content-Type": "application/json",
+        },
+    };
+    try{
+        const { data } = await axios.post(
+            "http://localhost:9999/db/ClientUsers/getUserByUsername",
+            { UserName: username },
+            config,
+        );
+        return data; 
+    }catch(error){
+        console.log(error);
+        return error;
+    }
+}
+const findEmployeeById = async(id) => {
     const config = {
         header: {
             "Content-Type": "application/json",
@@ -69,4 +76,22 @@ const findById = async(id) => {
         next(error);
     }
 }
-module.exports = {User, findOne, findById}; 
+const findClientById = async(id) => {
+    const config = {
+        header: {
+            "Content-Type": "application/json",
+        },
+    };
+    try{
+        const { data } = await axios.post(
+            "http://localhost:9999/db/clientUsers/getUser",
+            { ClientID: id },
+            config,
+        );
+        return data; 
+    }catch(error){
+        console.log(error);
+        next(error);
+    }
+}
+module.exports = {User, findOneEmployee, findOneClient, findEmployeeById, findClientById}; 
