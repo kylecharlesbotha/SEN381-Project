c56355136b3c9f628ca9242824872066808d9fc5
Kyle Botha <576383@student.belgiumcampus.ac.za>
2021-05-17 18:57:37 +0200

    Add forgot password middleware

--- Web App/Server/src/controllers/auth.js
+++ Web App/Server/src/controllers/auth.js
@@ -76,6 +76,46 @@ exports.loginClient = async (req, res, next) => {
         next(error);
     };
 };
+exports.forgotpassword = async(req, res, next) => {
+    const {username} = req.body; 
+    
+    try{
+        const userobj = await findOneClient(username) || await findOneEmployee(username); 
+        //
+        const user = new User(userobj);
+        console.dir(user)
+        if (!user){
+            return next(new ErrorResponse("Username Error",404)); 
+        };
+        const resetToken = await user.getResetPasswordToken(); 
+        
+
+        const resetUrl = `http:localhost:${process.env.FRONTEND_PORT}/passwordreset/${resetToken}`;
+        console.log(resetUrl);
+        const message = `
+        <h1>Password Reset Link</h1>
+        <a href=${resetUrl} clicktracking=off>${resetUrl}></a>
+        `; 
+
+        try{
+            // await sendEmail({
+            //     to: user.email, 
+            //     subject: "Password Reset Request", 
+            //     text: message
+            // });
+            console.log("1234");
+            res.status(200).json({success: true, data: "Email sent."});
+        }catch(error){
+            //user.resetPasswordToken = undefined; 
+           // user.resetPasswordExpire = undefined; 
+            await user.save(); 
+            return next(new ErrorResponse("Email Error",500));
+        }
+    }catch(error){
+        return next(error);
+    }
+
+}
 const sendToken = (user, statusCode, res) => {
     const token = user.getSignedToken();
     res.status(statusCode).json({success: true, token});
