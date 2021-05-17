94b15c4fcd5fbd6fd3023c5a716a2cef733eec29
Kyle Botha <576383@student.belgiumcampus.ac.za>
2021-05-17 14:51:18 +0200

    Add insert satisfaction functionality to

--- Web App/Server/src/models/Satisfaction.js
+++ Web App/Server/src/models/Satisfaction.js
@@ -13,4 +13,18 @@ const getAllSatisfactions = async() => {
         return error;
     }
 }
-module.exports = getAllSatisfactions; 
+const addOneSatisfaction = async(obj) => {
+    const config = {
+        header: {
+            "Content-Type": "application/json",
+        },
+    };
+    try{
+        const { data } = await axios.post("http://localhost:9999/db/satisfaction/addSatisfactions",obj, config);
+        return data; 
+    }catch(error){
+        console.log(error);
+        return error;
+    }
+}
+module.exports = {getAllSatisfactions, addOneSatisfaction}; 
