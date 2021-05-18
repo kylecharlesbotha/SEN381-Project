1576bc588ef4a8e5e9af02303e1e1ad98f125ceb
Kyle Botha <576383@student.belgiumcampus.ac.za>
2021-05-18 04:28:17 +0200

    Add route for client tickets

--- InternalDatabaseAPI/src/routers/ticket.js
+++ InternalDatabaseAPI/src/routers/ticket.js
@@ -1,7 +1,7 @@
 const router = new require("express").Router()
 const Crud = require("../models/crud");
 TicketCrud = new Crud("tblTicket", "TicketID");
-
+const execSQL = require("../db/db");
 router.get("/db/tickets/getTickets",async(req,res,next)=>{
     try{
         res.send(await TicketCrud.readAll());
@@ -24,5 +24,11 @@ router.post("/db/tickets/updateTicket",async(req,res,next)=>{
         next(error);
     }
 })
-
+router.post("/db/tickets/getClientTickets",async(req,res,next)=>{
+    const sql = `SELECT * FROM tblTicket WHERE ClientID='${req.body.ClientID}'`
+    console.log(sql);
+    const data = await execSQL(sql);
+     
+    res.send(data.recordset);
+})
 module.exports = router
